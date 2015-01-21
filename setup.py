53e8382b58e396cbe6cb416cb93343a4aa1db466
Fraser Nevett <frasern@gmail.com>
2015-01-21 17:44:07 +0000

    Added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+
+setup(
+    name = "lrs",
+    version = "0.0.0",
+    author = "ADL",
+    packages=['lrs'],
+    install_requires=[str(ir.req) for ir in install_reqs],
+)
