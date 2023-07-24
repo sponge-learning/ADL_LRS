63c738526eff30de84ec0e909b28b5582886943e
vvm46Vasylyk <volodumyr.vasylyk@spongelearning.com>
2023-07-24 16:06:58 +0200

    SPRK-2368: Try to fix ADL urls

--- adl_lrs/urls.py
+++ adl_lrs/urls.py
@@ -1,31 +1,33 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
-
+from . import views
 # Uncomment the next two lines to enable the admin (imports admin module in each app):
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/xAPI/')),
-    url(r'^XAPI/', include('lrs.urls')),
-    url(r'^xapi/', include('lrs.urls')),
-    url(r'^xAPI/', include('lrs.urls')),
+    url(r'^XAPI/', include('lrs.urls', namespace='lrs')),
+    url(r'^xapi/', include('lrs.urls', namespace='lrs')),
+    url(r'^xAPI/', include('lrs.urls', namespace='lrs')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 # Login and logout patterns
-urlpatterns += patterns('',
-  url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
-  url(r'^accounts/logout/$', 'lrs.views.logout_view', name="logout"),
-)
+urlpatterns += [
+    url(r'^accounts/login/$', auth_views.login, name="login"),
+    url(r'^accounts/logout/$', views.logout_view, name="logout"),
+]
 
 # Allows admins to view attachments in admin console
 if settings.DEBUG:
-  urlpatterns += patterns('',
-      url(r'^media/attachment_payloads/(?P<path>.*)$', 'lrs.views.admin_attachments'),
- )
+    urlpatterns += [
+        url(r'^media/attachment_payloads/(?P<path>.*)$', views.admin_attachments),
+    ]
