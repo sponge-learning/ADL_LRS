88f413879f5d758044bba1196fb775efa3bea476
David Bramwell <david.bramwell@spongelearning.com>
2023-07-28 12:56:49 +0100

    Don't import patterns

--- oauth_provider/compat.py
+++ oauth_provider/compat.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 import django
 
-from django.conf.urls import patterns, url, include
-
-
 
 # in Django>=1.5 CustomUser models can be specified
 if django.VERSION >= (1, 5):
